da573b562c11a7d665e1361c6e578e3d842d157f
mex3 <3meandme@gmail.com>
2016-04-09 10:34:36 +0300

    Добавлено открытие и закрытие файлов

--- 424.py
+++ 424.py
@@ -5,3 +5,10 @@ MonthNames = ('', 'January', 'February', 'March', 'April', 'May', 'June', 'July'
 DayOfTheWeek = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
 MonthNums = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 DayNums = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
+
+
+fin = open("input.txt", "r")
+fout = open("output.txt", "w")
+
+fin.close()
+fout.close()
