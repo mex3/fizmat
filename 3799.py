68f7d21f3a8fca1f0f1c3e348ed88fa5fbb2c815
mex3 <3meandme@gmail.com>
2016-04-09 16:52:25 +0300

    Create 3799.py

---
+++ 3799.py
@@ -0,0 +1,12 @@
+#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3799#1
+#РЕКУРСИЯ! Должна быть рекурсия!!!!
+
+a = input()
+b=1 #Делитель
+while a>b:
+  if a//b
+    ans = "NO"#Ответ
+    break
+  else:
+    ans = "YES"#Ответ
+print(c)
