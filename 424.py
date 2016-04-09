123117d4187de381f3e01d5f78382ee4c95e840a
mex3 <3meandme@gmail.com>
2016-04-09 10:30:37 +0300

    Update 424.py

--- 424.py
+++ 424.py
@@ -1,1 +1,7 @@
 #http://informatics.mccme.ru/moodle/mod/statements/view.php?chapterid=424#1
+
+#вы можете писать комментарии, что именно вы реализовали
+MonthNames = ('', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+DayOfTheWeek = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+MonthNums = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+DayNums = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
