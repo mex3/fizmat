5f51ba168511c2b000a19e98d6a1f1a3dc4df244
mex3 <3meandme@gmail.com>
2016-04-09 16:49:43 +0300

    Create 197.py

---
+++ 197.py
@@ -0,0 +1,10 @@
+l = input()               
+
+x, y = list(map(int, input().split())
+m = []
+a = list(range(l, 0, 1))
+for i in range(l, -1, -1):
+    m.append(str(a[x[i]]))
+    del a[x[i]]
+m.reverse()
+print(' '.join(m))
