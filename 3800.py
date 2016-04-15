a22a2d00e10f5450c232a8dba0a168f5dfb78c47
piroman01 <alexair01@mail.ru>
2016-04-15 17:35:50 +0400

    Update 3800.py

--- 3800.py
+++ 3800.py
@@ -1,2 +1,19 @@
 #http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3800#1
 #Рекурсия!
+
+
+def power(a, n):
+   if n == 0: 
+      return 1 
+   return(a * power(a, n - 1))
+
+a = input()
+try: 
+   a = int(a) 
+except: 
+   a = float(a) 
+   n = int(input()) 
+   print(power(a, n)) 
+else: 
+   n = int(input()) 
+   print(power(a, n)) 
