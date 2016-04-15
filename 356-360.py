9086580620dc723d62ae7b4dea6019eaae5f80d5
piroman01 <alexair01@mail.ru>
2016-04-15 17:28:31 +0400

    Update 356-360.py

--- 356-360.py
+++ 356-360.py
@@ -1,7 +1,7 @@
 #http://informatics.mccme.ru/mod/statements/view.php?id=15232#1
 #решить несколько задач из 5
 
-#356
+№356
 
 n, m = input().split()
 n = int(n)
@@ -9,17 +9,37 @@ m = int(m)
 a = []
 for i in range(n):
     a.append(input().split())
-    
 for i in range(n):
     for k in range(m):
         a[i][k] = int(a[i][k])
-
 summ = []
 for i in range(n):
     summ.append(sum(a[i]))
-
 print(max(summ))
 print(summ.index(max(summ)))
 
 
-#357
+№357
+
+n, m = input().split()
+n = int(n)
+m = int(m)
+a = []
+for i in range(n):
+    a.append(input().split())
+for i in range(n):
+    for k in range(m):
+        a[i][k] = int(a[i][k])
+maxx=[]
+for i in range(n):
+    maxx.append(max(a[i]))
+max0=max(maxx)
+print(max0)
+max1 = maxx.index(max0)
+max2 = a[max1].index(max0)
+print(max1, max2)
+
+
+№358
+
+
