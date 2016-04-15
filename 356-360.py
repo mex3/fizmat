d277ec1eabfc7db0c17fb673a808e546ab7ce21b
SvetaZabarina <motther@mail.ru>
2016-04-15 23:15:07 +0300

    Update 356-360.py

--- 356-360.py
+++ 356-360.py
@@ -74,3 +74,34 @@ for i in m:
     i = int()
 u = max(m)
 print(count(u))
+#359
+a, b = map(int,input().split(" "))
+r = [ ]
+for i in range(a):
+    q = list(map(int,input().split(' ')))
+    m = max(q)
+    r.append(m)
+w = max(r)
+o = 0
+for i in range(a):
+    if r[i]== w:
+        o += 1
+print(o)
+
+#360
+a, b = map(int,input().split(" "))
+r = [ ]
+for i in range(a):
+    q = list(map(int,input().split(' ')))
+    m = max(q)
+    r.append(m)
+w = max(r)
+o = 0
+e = [ ]
+for i in range(a):
+    if r[i]== w:
+        o += 1
+        e.append(i)
+print(o)
+for i in range(len(e)):
+    print(e[i])
