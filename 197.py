7806b364d11fadcbb5f4aba27234daaca7121e11
piroman01 <alexair01@mail.ru>
2016-04-15 17:53:15 +0400

    Update 197.py

--- 197.py
+++ 197.py
@@ -8,3 +8,18 @@ for i in range(l, -1, -1):
     del a[x[i]]
 m.reverse()
 print(' '.join(m))
+
+
+
+
+№197
+k = int(input())                         
+r = q = list(map(int, input().split())) 
+m = [] 
+n = list(range(k, 0, -1)) 
+for i in range(k-1, -1, -1): 
+    m.append(str(n[r[i]])) 
+    del n[r[i]] 
+m.reverse() 
+print(' '.join(m)) 
+
