5a8e0886b066e55df0ae1697a9f4fec214666bb2
Maxpuchin <max.puchin@gmail.com>
2016-04-15 18:37:29 +0300

    Update 356-360.py

--- 356-360.py
+++ 356-360.py
@@ -56,3 +56,20 @@ for i in m:
     i = int()
 u = m.index(max(m))
 if u - 2 
+
+#359
+a = int(input())#number of athlets
+c = []
+while a > 0:
+    a-=1
+    b = map(str, input().split())
+    c.append(b)
+print(b)#print list of imposed data
+m = []
+for i in b:
+    n = ' '.join(i)
+    m.append(n)
+for i in m:
+    i = int()
+u = max(m)
+print(count(u))
