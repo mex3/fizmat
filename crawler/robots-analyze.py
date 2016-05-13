4386f32ac1607c830d830163b494def8e3092066
Emil <Emil>
2016-05-13 18:43:27 +0300

    robots-analyse

--- crawler/robots-analyze.py
+++ crawler/robots-analyze.py
@@ -1,4 +1,30 @@
-#читает ссылку из linksraw.txt
-#скачивает с этого сайта robots.txt
-#проверяет, можно ли эту ссылку скачивать по правилам из robots.txt
-#если можно - дописывает ее в конец linkslist.txt
+
+def remove_string(filename, string):
+    rst = []
+    with open(filename) as fd:
+        t = fd.read()
+        for line in t.splitlines():
+            if line != string:
+                rst.append(line)
+
+    with open(filename, 'w') as fd:
+        fd.write('\n'.join(rst))
+        fd.write('\n')
+
+f = open('linksraw.txt', 'r')#РЅРµ СѓРјРµРµС‚ СЃРєР°С‡РёРІР°С‚СЊ robots.txt
+d = open('linkslist.txt', 'a')
+x = open('robots.txt')
+for paths in x:
+    if 'Disallow:' in paths:
+        paths = paths.strip()
+        paths = ''.join(paths.split(':')[1:])
+        for links in f:
+            if paths in links:
+                print(links)
+                remove_string('linksraw.txt',links)
+f.close()
+f = open('linksraw.txt', 'r')
+for links in f:
+    d.write(links)
+d.close()
+f.close()
