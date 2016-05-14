0cc043bb95c41a971cd026bc3b64693ae2257b04
NIK <niksgaara@rambler.ru>
2016-05-14 11:44:38 +0400

    Update downloader.py

--- crawler/downloader.py
+++ crawler/downloader.py
@@ -3,3 +3,4 @@ f=open("linkslist.txt", "r")
 for x in f:
     subprocess.call(["C:\Users\mihalev_n\Documents\cURL\curl.exe", x, '-o', "file_#1.txt"],shell= True)#start cURL executable
 f.close()
+#http://www.py-my.ru/post/4bfb3c6a1d41c846bc00009b
