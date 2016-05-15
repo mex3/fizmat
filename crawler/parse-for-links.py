1970d2a0b98ee2a9b66faddf8861c77eb8d351a7
NIK <niksgaara@rambler.ru>
2016-05-15 12:56:45 +0300

    Update parse-for-links.py

--- crawler/parse-for-links.py
+++ crawler/parse-for-links.py
@@ -1,6 +1,6 @@
 import re
 p = re.compile('<a\s*href\s*=\s*["](.*)["]\s*>')
-f = open('file_#1.txt', 'r', encoding= 'utf-8')
+f = open('file_#1.txt', 'r', encoding= 'ANSI')
 d = open('linksraw.txt', 'w')
 for line in f:
     x = ''.join(p.findall(line))
